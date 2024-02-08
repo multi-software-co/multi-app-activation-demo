c9f231dd3f64384c4113d731fcd2183689d9cb11
Art Lasovsky <artlasovsky@gmail.com>
2024-02-08 15:42:58 +0100

    Initial Commit

---
+++ MultiAppActivationDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MultiAppActivationDemo
+//
+//  Created by Art Lasovsky on 08/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MultiAppActivationDemo/MultiAppActivationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiAppActivationDemoApp.swift
+//  MultiAppActivationDemo
+//
+//  Created by Art Lasovsky on 08/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MultiAppActivationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
